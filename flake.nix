747d1fe40965367f32d17d3adbac46189b3f4a87
a-kenji <aks.kenji@protonmail.com>
2023-05-17 23:43:12 +0200

    rm: cruft

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "flk - a tui for your flakes.";
+  description = "tui-term - a pseudoterminal widget for ratatui";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -9,19 +9,11 @@
     inputs.flake-utils.follows = "flake-utils";
   };
 
-  inputs.crane = {
-    url = "github:ipetkov/crane";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.rust-overlay.follows = "rust-overlay";
-    inputs.flake-utils.follows = "flake-utils";
-  };
-
   outputs = {
     self,
     nixpkgs,
     flake-utils,
     rust-overlay,
-    crane,
   }:
     flake-utils.lib.eachDefaultSystem
     (system: let
@@ -35,16 +27,14 @@
         inherit system overlays;
       };
       src = self;
-      name = "flk";
       RUST_TOOLCHAIN = src + "/rust-toolchain.toml";
       cargoTOML = builtins.fromTOML (builtins.readFile (src + "/Cargo.toml"));
-      inherit (cargoTOML.workspace.package) version;
+      inherit (cargoTOML.package) version name;
       rustToolchainTOML = rustPkgs.rust-bin.fromRustupToolchainFile RUST_TOOLCHAIN;
       rustToolchainDevTOML = rustToolchainTOML.override {
         extensions = ["rustfmt" "clippy" "rust-analysis" "rust-docs"];
         targets = [];
       };
-      buildAndTestSubdir = "./crate/flk";
       gitDate = self.lastModifiedDate;
       gitRev = self.shortRev or "Not committed yet.";
       cargoLock = {
@@ -143,37 +133,13 @@
       actionlintInputs = [
         pkgs.actionlint
       ];
-      targetDir = "target/${pkgs.rust.toRustTarget pkgs.stdenv.targetPlatform}/release";
-      assetDir = "crate/flk/${targetDir}/assets";
-      postInstall = ''
-        # install the manpage
-        installManPage ${assetDir}/${name}.1
-        # explicit behavior
-        cp ${assetDir}/${name}.bash ./completions.bash
-        installShellCompletion --bash --name ${name}.bash ./completions.bash
-        cp ${assetDir}/${name}.fish ./completions.fish
-        installShellCompletion --fish --name ${name}.fish ./completions.fish
-        cp ${assetDir}/_${name} ./completions.zsh
-        installShellCompletion --zsh --name _${name} ./completions.zsh
-      '';
-      ASSET_DIR = "./target/assets";
-      args = {inherit src buildInputs nativeBuildInputs;};
-      # crane
-      buildCrane = ((crane.mkLib pkgs).overrideToolchain rustToolchainTOML).buildPackage;
-      buildCraneArtifacts = ((crane.mkLib pkgs).overrideToolchain rustToolchainTOML).buildDepsOnly args;
-      includeMigrationsFilter = path: _type: builtins.match ".*/migrations/*" path != null;
-      migrationsOrCargo = path: type:
-        (includeMigrationsFilter path type) || ((crane.mkLib pkgs).filterCargoSources path type);
     in rec {
       devShells = {
         default = (pkgs.mkShell.override {inherit stdenv;}) {
           buildInputs = shellInputs ++ fmtInputs ++ devInputs ++ buildInputs ++ nativeBuildInputs;
-          inherit name ASSET_DIR;
-          FLK_LOG = "debug";
-          DATABASE_URL = "/tmp/flk/flk-database.db";
+          inherit name;
           RUST_BACKTRACE = true;
           RUSTFLAGS = "-C linker=clang -C link-arg=-fuse-ld=${pkgs.mold}/bin/mold -C target-cpu=native";
-          # RUSTFLAGS = "-C linker=clang -C link-arg=--ld-path=${pkgs.mold}/bin/mold -C link-arg=-Wl,--warn-unresolved-symbols -C debuginfo=1";
           shellHook = ''
             export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath buildInputs}"
           '';
@@ -200,7 +166,6 @@
             GIT_DATE = gitDate;
             GIT_REV = gitRev;
             doCheck = false;
-            ASSET_DIR = "${targetDir}/assets/";
             inherit
               name
               version
@@ -209,38 +174,8 @@
               nativeBuildInputs
               buildInputs
               cargoLock
-              buildAndTestSubdir
-              postInstall
               ;
           };
-        crane = buildCrane (args
-          // {
-            cargoDepsName = name;
-            GIT_DATE = gitDate;
-            GIT_REV = gitRev;
-            doCheck = false;
-            ASSET_DIR = "${targetDir}/assets/";
-            src = pkgs.lib.cleanSourceWith {
-              src = (crane.mkLib pkgs).path ./.; # The original, unfiltered source
-              filter = migrationsOrCargo;
-            };
-            inherit
-              name
-              version
-              stdenv
-              buildAndTestSubdir
-              # src
-
-              # nativeBuildInputs
-
-              # buildInputs
-
-              # cargoLock
-
-              # postInstall
-
-              ;
-          });
       };
       apps.default = {
         type = "app";
