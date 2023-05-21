ac17a6a5e3e2906876f83d5bc4bb052a7a5fe8b3
a-kenji <aks.kenji@protonmail.com>
2023-05-21 20:54:25 +0200

    add: vttest_02_01

--- benches/benches.rs
+++ benches/benches.rs
@@ -11,10 +11,17 @@ static SIMPLE_LS_ACTIONS: Lazy<Vec<Action>> = Lazy::new(|| {
     parser.parse_as_vec(simple_ls)
 });
 
-fn simple_ls() {
+static VTTEST_02_01: Lazy<Vec<Action>> = Lazy::new(|| {
+    let simple_ls = include_bytes!("../test/typescript/vttest_02_01.typescript");
+    let mut parser = termwiz::escape::parser::Parser::new();
+    parser.parse_as_vec(simple_ls)
+});
+
+#[inline]
+fn render_typescript(actions: Vec<Action>) {
     let backend = TestBackend::new(80, 24);
     let mut terminal = Terminal::new(backend).unwrap();
-    let pseudo_term = PseudoTerm::new(&SIMPLE_LS_ACTIONS);
+    let pseudo_term = PseudoTerm::new(&actions);
     terminal
         .draw(|f| {
             f.render_widget(pseudo_term, f.size());
@@ -23,8 +30,19 @@ fn simple_ls() {
     let _view = terminal.backend().to_string();
 }
 
+#[inline]
+fn simple_ls() {
+    render_typescript(SIMPLE_LS_ACTIONS.to_vec())
+}
+
+#[inline]
+fn vttest_02_01() {
+    render_typescript(VTTEST_02_01.to_vec())
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("simple ls", |b| b.iter(simple_ls));
+    c.bench_function("vttest_02_01", |b| b.iter(vttest_02_01));
 }
 
 criterion_group!(benches, criterion_benchmark);
