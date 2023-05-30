d76a7ddf89602e5cce2f358e6866820c5b0383c9
a-kenji <aks.kenji@protonmail.com>
2023-05-30 19:40:31 +0200

    chore: init simple_mux

---
+++ examples/simple_mux.rs
@@ -0,0 +1,146 @@
+use std::{
+    io,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    execute,
+    style::ResetColor,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use portable_pty::{CommandBuilder, NativePtySystem, PtySize, PtySystem};
+use ratatui::text::Line;
+use ratatui::{
+    backend::Backend,
+    layout::Alignment,
+    style::{Modifier, Style},
+    widgets::{Block, Borders, Paragraph},
+    Frame,
+};
+use ratatui::{backend::CrosstermBackend, Terminal};
+use tui_term::widget::PseudoTerm;
+use vt100::Screen;
+
+fn main() -> std::io::Result<()> {
+    let mut stdout = io::stdout();
+    execute!(stdout, ResetColor)?;
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let pty_system = NativePtySystem::default();
+    let cwd = std::env::current_dir().unwrap();
+    let mut cmd = CommandBuilder::new("top");
+    cmd.cwd(cwd);
+
+    let pair = pty_system
+        .openpty(PtySize {
+            rows: 24,
+            cols: 80,
+            pixel_width: 0,
+            pixel_height: 0,
+        })
+        .unwrap();
+    // Wait for the child to complete
+    std::thread::spawn(move || {
+        let mut child = pair.slave.spawn_command(cmd).unwrap();
+        let _child_exit_status = child.wait().unwrap();
+        drop(pair.slave);
+    });
+
+    let mut reader = pair.master.try_clone_reader().unwrap();
+    let parser = Arc::new(RwLock::new(vt100::Parser::new(24, 80, 0)));
+
+    {
+        let parser = parser.clone();
+        std::thread::spawn(move || {
+            // Consume the output from the child
+            // Can't read the full buffer, since that would wait for EOF
+            let mut buf = [0u8; 8192];
+            loop {
+                let size = reader.read(&mut buf).unwrap();
+                if size == 0 {
+                    break;
+                }
+                if !buf.is_empty() {
+                    let mut parser = parser.write().unwrap();
+                    parser.process(&buf);
+                }
+            }
+        });
+    }
+
+    {
+        // Drop writer on purpose
+        let _writer = pair.master.take_writer().unwrap();
+    }
+    drop(pair.master);
+
+    run(&mut terminal, parser)?;
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
+}
+
+fn run<B: Backend>(
+    terminal: &mut Terminal<B>,
+    parser: Arc<RwLock<vt100::Parser>>,
+) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, parser.read().unwrap().screen()))?;
+
+        // Event read is blocking
+        if event::poll(Duration::from_millis(10))? {
+            // It's guaranteed that the `read()` won't block when the `poll()`
+            // function returns `true`
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    if let KeyCode::Char('q') = key.code {
+                        return Ok(());
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, screen: &Screen) {
+    let chunks = ratatui::layout::Layout::default()
+        .direction(ratatui::layout::Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                ratatui::layout::Constraint::Percentage(0),
+                ratatui::layout::Constraint::Percentage(100),
+                ratatui::layout::Constraint::Min(1),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+    // TODO: find a good continuuous test cmd
+    let title = Line::from("[ Running: ls ]");
+    let block = Block::default()
+        .borders(Borders::ALL)
+        .title(title)
+        .style(Style::default().add_modifier(Modifier::BOLD));
+    let pseudo_term = PseudoTerm::new(screen).block(block);
+    f.render_widget(pseudo_term, chunks[1]);
+    let block = Block::default().borders(Borders::ALL);
+    f.render_widget(block, f.size());
+    let explanation = "Press q to exit".to_string();
+    let explanation = Paragraph::new(explanation)
+        .style(Style::default().add_modifier(Modifier::BOLD | Modifier::REVERSED))
+        .alignment(Alignment::Center);
+    f.render_widget(explanation, chunks[2]);
+}
