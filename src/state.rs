dd5e5e5c45b43f22de91d82eb46607f90332594a
a-kenji <aks.kenji@protonmail.com>
2024-08-07 15:04:12 +0200

    refactor: remove deprecated `buf.get_mut()` function

--- src/state.rs
+++ src/state.rs
@@ -25,7 +25,7 @@ pub fn handle<S: Screen>(term: &PseudoTerminal<S>, area: Rect, buf: &mut Buffer)
             }
 
             if let Some(screen_cell) = screen.cell(row, col) {
-                let cell = buf.get_mut(buf_col, buf_row);
+                let cell = &mut buf[(buf_col, buf_row)];
                 screen_cell.apply(cell);
             }
         }
@@ -34,7 +34,7 @@ pub fn handle<S: Screen>(term: &PseudoTerminal<S>, area: Rect, buf: &mut Buffer)
     if !screen.hide_cursor() && term.cursor.show {
         let (c_row, c_col) = screen.cursor_position();
         if (c_row + row_start) < area_rows && (c_col + col_start) < area_cols {
-            let c_cell = buf.get_mut(c_col + col_start, c_row + row_start);
+            let c_cell = &mut buf[(c_col + col_start, c_row + row_start)];
             if let Some(cell) = screen.cell(c_row, c_col) {
                 if cell.has_contents() {
                     let style = term.cursor.overlay_style;
